5251b745400d4e6b02b26df0b21b606f455e84ef
leleact <leleact@gmail.com>
2015-04-02 21:02:44 +0800

    relock

--- relock/testrelock.c
+++ relock/testrelock.c
@@ -3,7 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+
 #define FILENAME "filefortest"
+
+int lock_reg(int, int, int, off_t, int, off_t);
+
+#define read_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define readw_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define write_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define writew_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define un_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
 
 int main() {
 	int fd;
@@ -23,9 +37,9 @@ int main() {
 	struct flock sFlock;
 	memset(&sFlock, 0x00, sizeof(struct flock));
 	sFlock.l_type = F_WRLCK;
-	sFlock.l_start = 10;
+	sFlock.l_start = 0;
 	sFlock.l_whence = SEEK_SET;
-	sFlock.l_len = 10;
+	sFlock.l_len = 0;
 
 	if (-1 == fcntl(fd, F_SETLKW, &sFlock)) {
 		fprintf(stderr, "fcntl err!\n");	
