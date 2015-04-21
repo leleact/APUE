1fa648a1bcaa9539da8cf4d2d363e2fd29282993
leleact <leleact@gmail.com>
2015-04-21 21:24:24 +0800

    unp

---
+++ socket/lele_test_strecho.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SERV_PORT 8080
+#define LISTENQ 5
+
+void str_echo(int);
+
+int main(int argc, char **argv)
+{
+	int listenfd, connfd;
+	pid_t childpid;
+	socklen_t chilen;
+
+	struct sockaddr_in cliaddr, servaddr;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == listenfd)
+	{
+		fprintf(stderr, "socket error\n");	
+		exit(1);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	int ret = bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	if (-1 == ret)
+	{
+		fprintf(stderr, "bind error\n");	
+		exit(1);
+	}
+
+	ret = listen(listenfd, LISTENQ);
+	if (-1 == ret)
+	{
+		fprintf(stderr, "listen error\n");	
+		exit(1);
+	}
+
+	for (;;)
+	{
+		chilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &chilen);
+		if ((childpid = fork()) < 0)
+		{
+			fprintf(stderr, "fork error\n");	
+			exit(1);
+		}
+		else if (childpid == 0)
+		{
+			close(listenfd);
+			str_echo(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+}
+
+void str_echo(int sockfd)
+{
+	ssize_t n;
+	char buf[4096];
+
+again:
+	while((n = read(sockfd, buf, 4096)) > 0)
+	{
+		write(sockfd, buf, n);
+	}
+
+	if (n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+	{
+		fprintf(stderr, "str_echo error\n");	
+		exit(1);
+	}
+}
