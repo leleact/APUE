20ac05cdf7884a80fe67dcb6263cf6ab6662e775
leleact <leleact@gmail.com>
2015-05-14 00:56:32 +0800

    echo

--- socket/lele_test_strcli.c
+++ socket/lele_test_strcli.c
@@ -1,59 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 void str_cli(FILE *, int);
 
-int main(int argc, char **argv)
-{
-	int sockfd;
-	struct sockaddr_in servaddr;
-	
-	if (argc < 2)
-	{
-		fprintf(stdout, "usage: tcpcli <IPaddress>\n");	
-		exit(1);
-	}
+int main(int argc, char **argv) {
+  int sockfd;
+  int ret = 0;
+  struct sockaddr_in servaddr;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (-1 == sockfd)
-	{
-		fprintf(stderr, "socket error\n");
-		exit(1);
-	}
+  if (argc != 3) {
+    fprintf(stdout, "usage: clien <IP address> <IP port>\n");
+    exit(1);
+  }
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
-	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+  int ip_port = atoi(*(argv + 2));
 
-	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == sockfd) {
+    fprintf(stderr, "socket error\n");
+    exit(1);
+  }
 
-	str_cli(stdin, sockfd);
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(ip_port);
+  inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
-	exit(0);
+  ret = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  if (-1 == ret) {
+    fprintf(stderr, "connect error\n");
+    exit(1);
+  }
+
+  str_cli(stdin, sockfd);
+
+  exit(0);
 }
 
-void str_cli(FILE *fp, int sockfd)
-{
-	char sendline[4096];
-	char recvline[4096];
+void str_cli(FILE *fp, int sockfd) {
+  char sendline[4096];
+  char recvline[4096];
 
-	while(fgets(sendline, 4096, fp) != NULL)
-	{
-		write(sockfd, sendline, strlen(sendline));	
+  while(fgets(sendline, 4096, fp) != NULL) {
+    write(sockfd, sendline, strlen(sendline));
 
-		if (readline(sockfd, recvline, 4096) == 0)
-		{
-			fprintf(stderr, "str_cli: server terminated prematurely\n");	
-			exit(1);
-		}
+    memset(recvline, 0x00, sizeof(recvline));
+    if (read(sockfd, recvline, 4096) == 0) {
+      fprintf(stderr, "error: server terminated prematurely\n");
+      exit(1);
+    }
 
-		fputs(recvline, stdout);
-	}
+    fputs(recvline, stdout);
+  }
 }
--- socket/lele_test_strecho.c
+++ socket/lele_test_strecho.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
@@ -7,80 +8,75 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define SERV_PORT 8080
 #define LISTENQ 5
 
 void str_echo(int);
 
-int main(int argc, char **argv)
-{
-	int listenfd, connfd;
-	pid_t childpid;
-	socklen_t chilen;
+int main(int argc, char **argv) {
+  int listenfd, connfd;
+  pid_t childpid;
+  socklen_t chilen;
 
-	struct sockaddr_in cliaddr, servaddr;
+  if (argc != 2) {
+    fprintf(stdout, "please use : server <port>\n");
+    exit(1);
+  }
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (-1 == listenfd)
-	{
-		fprintf(stderr, "socket error\n");	
-		exit(1);
-	}
+  int server_port = atoi(*(argv + 1));
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(SERV_PORT);
+  struct sockaddr_in cliaddr, servaddr;
 
-	int ret = bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-	if (-1 == ret)
-	{
-		fprintf(stderr, "bind error\n");	
-		exit(1);
-	}
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == listenfd) {
+    fprintf(stderr, "socket error\n");
+    exit(1);
+  }
 
-	ret = listen(listenfd, LISTENQ);
-	if (-1 == ret)
-	{
-		fprintf(stderr, "listen error\n");	
-		exit(1);
-	}
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(server_port);
 
-	for (;;)
-	{
-		chilen = sizeof(cliaddr);
-		connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &chilen);
-		if ((childpid = fork()) < 0)
-		{
-			fprintf(stderr, "fork error\n");	
-			exit(1);
-		}
-		else if (childpid == 0)
-		{
-			close(listenfd);
-			str_echo(connfd);
-			exit(0);
-		}
-		close(connfd);
-	}
+  int ret = bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  if (-1 == ret) {
+    fprintf(stderr, "bind error\n");
+    exit(1);
+  }
+
+  ret = listen(listenfd, LISTENQ);
+  if (-1 == ret) {
+    fprintf(stderr, "listen error\n");
+    exit(1);
+  }
+
+  for (;;) {
+    chilen = sizeof(cliaddr);
+    connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &chilen);
+    if ((childpid = fork()) < 0) {
+      fprintf(stderr, "fork error\n");
+      exit(1);
+    } else if (childpid == 0) {
+      close(listenfd);
+      str_echo(connfd);
+      exit(0);
+    }
+    close(connfd);
+  }
 }
 
-void str_echo(int sockfd)
-{
-	ssize_t n;
-	char buf[4096];
+void str_echo(int sockfd) {
+  ssize_t n;
+  char buf[4096];
 
 again:
-	while((n = read(sockfd, buf, 4096)) > 0)
-	{
-		write(sockfd, buf, n);
-	}
+  while((n = read(sockfd, buf, 4096)) > 0) {
+    write(sockfd, buf, n);
+  }
 
-	if (n < 0 && errno == EINTR)
-		goto again;
-	else if (n < 0)
-	{
-		fprintf(stderr, "str_echo error\n");	
-		exit(1);
-	}
+  if (n < 0 && errno == EINTR)
+    goto again;
+  else if (n < 0) {
+    fprintf(stderr, "str_echo error\n");
+    exit(1);
+  }
 }
